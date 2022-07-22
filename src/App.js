b09262b4bc89ab919deaec6bb355f9ae5877645b
unknown <ribeirojoaolucas68@gmail.com>
2022-07-22 15:08:29 -0300

    feature: create a registration form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-import { LoginForm } from "./components/LoginForm";
+import { RegistrationForm } from "./components/RegistrationForm";
 
 function App() {
   return (
     <div className="App">
       <div className="center">
-        <LoginForm />
+        <RegistrationForm />
       </div>
     </div>
   );
